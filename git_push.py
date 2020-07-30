1e5c8d74d4f8fa577457c89200f050c86c228227
Kathryn Chapman <kathrynchapman@Kathryns-Air.fritz.box>
2020-07-30 21:48:16 +0200

    test

--- git_push.py
+++ git_push.py
@@ -4,7 +4,12 @@ cmd = 'git add .'
 os.popen(cmd).read()
 cmd = 'git status'
 os.popen(cmd).read()
-cmd = 'git commit -m "bug fixes"'
+inp = input("Message? Hit enter for simple 'bug fixes'\n\t")
+if inp:
+    message = inp
+else:
+    message = 'bug fixes'
+cmd = 'git commit -m "{}"'.format(message)
 os.popen(cmd).read()
 cmd = 'git push origin master'
 os.popen(cmd).read()
