60d405a5afde1dc553bafbb83a7337c85ef23982
Kathryn Chapman <kathrynchapman@Kathryns-Air.fritz.box>
2020-07-30 18:48:18 +0200

    bug fixes

--- exps_to_run.py
+++ exps_to_run.py
@@ -6,18 +6,11 @@ from beautifultable import BeautifulTable
 from datetime import datetime
 
 models = ['baseline', 'label_attention']
-
 num_epohcs = ['any int']
-
-# doc_batching = ['doc_batching_max', 'doc_batching_avg', 'no_doc_batching']
 doc_batching = ['doc_batching_max', 'no_doc_batching']
-
-ranking_loss = ['ranking_loss', 'no_ranking_loss']
-
+ranking_loss = ['ranking_loss', 'no_ranking_loss', 'weighted_ranking_loss']
 class_weights = ['dynamic_class_weights', 'static_class_weights', 'no_class_weights']
-# class_weights = ['class_weights', 'no_class_weights']
-loss_function = ['bce', 'bbce']
-
+loss_function = ['bce', 'bbce', 'none']
 
 
 def get_input(ref_type, str_type):
@@ -36,6 +29,7 @@ def get_input(ref_type, str_type):
         else:
             invalid = False
     return inp_type
+
 
 def fill_dict(exp_dict):
     blah = 'a'
@@ -81,10 +75,10 @@ def fill_dict(exp_dict):
                 if notes == 'exit':
                     break
                 exp_dict[inputs[0]][inputs[1]][inputs[2]][inputs[3]][inputs[4]][inputs[5]]['Notes'] = notes + \
-                                                                            " (last edited {})".format(dt_string)
+                                                                                                      " (last edited {})".format(
+                                                                                                          dt_string)
                 save(exp_dict, 'exp_dict.p')
                 continue
-
 
         MAP = input("Enter MAP: ")
         F1 = input("Enter F1: ")
@@ -98,14 +92,13 @@ def fill_dict(exp_dict):
         exp_dict[inputs[0]][inputs[1]][inputs[2]][inputs[3]][inputs[4]][inputs[5]]['R'] = float(R)
         exp_dict[inputs[0]][inputs[1]][inputs[2]][inputs[3]][inputs[4]][inputs[5]]['Entry Date'] = dt_string
 
-
         if notes or add_to_notes:
             exp_dict[inputs[0]][inputs[1]][inputs[2]][inputs[3]][inputs[4]][inputs[5]]['Notes'] = notes + add_to_notes
 
         save(exp_dict, 'exp_dict.p')
         done = True if input("Do you have another experiment to enter? [y, n]: ") == 'n' else False
         if not done:
-            print('-'*100)
+            print('-' * 100)
 
 
 def lookup(exp_dict):
@@ -129,6 +122,7 @@ def lookup(exp_dict):
         if not done:
             print('-' * 100)
 
+
 def viewall(exp_dict):
     '''
     # >>> from beautifultable import BeautifulTable
@@ -144,10 +138,10 @@ def viewall(exp_dict):
     :param exp_dict:
     :return:
     '''
-    table = BeautifulTable(maxwidth=150)
+    table = BeautifulTable(maxwidth=300)
     table.columns.header = ["Model", "#Epochs", "Doc Batching", "Ranking Loss", "Class Weights", "Loss Funct", "MAP",
                             "F1", "P", "R", "Notes", "Entry Date"]
-    table.column_widths = [10, 9, 15, 15, 15, 15, 7, 7, 7, 7, 20, 15]
+    table.column_widths = [15, 9, 15, 15, 23, 15, 7, 7, 7, 7, 20, 15]
     for m in exp_dict.keys():
         for n_ep in exp_dict[m].keys():
             for db in exp_dict[m][n_ep].keys():
@@ -160,18 +154,18 @@ def viewall(exp_dict):
                             r = exp_dict[m][n_ep][db][rl][cw][lf]['R']
                             notes = exp_dict[m][n_ep][db][rl][cw][lf]['Notes']
                             ed = exp_dict[m][n_ep][db][rl][cw][lf]['Entry Date']
-                            m_ = ' label  attn ' if m == 'label_attention' else m
+                            m_ = 'label attn' if m == 'label_attention' else m
                             rl_ = 'Y' if rl == 'ranking_loss' else 'N'
                             d = 'N' if db == 'no_doc_batching' else db[-3:]
                             c = 'N' if cw == 'no_class_weights' else cw
                             ed = 'N/A' if ed == 0.0 else ed
                             table.rows.append([m_, n_ep, d, rl_, c, lf, map, f1, p, r, notes, ed])
     print(table)
-                            # print("Results for {}, {}, {}, {}, {}, {}".format(m, n_ep, db, rl, cw, lf))
-                            # print("MAP:", exp_dict[m][n_ep][db][rl][cw][lf]['MAP'])
-                            # print("F1:", exp_dict[m][n_ep][db][rl][cw][lf]['F1'])
-                            # print("P:", exp_dict[m][n_ep][db][rl][cw][lf]['P'])
-                            # print("R:", exp_dict[m][n_ep][db][rl][cw][lf]['R'])
+    # print("Results for {}, {}, {}, {}, {}, {}".format(m, n_ep, db, rl, cw, lf))
+    # print("MAP:", exp_dict[m][n_ep][db][rl][cw][lf]['MAP'])
+    # print("F1:", exp_dict[m][n_ep][db][rl][cw][lf]['F1'])
+    # print("P:", exp_dict[m][n_ep][db][rl][cw][lf]['P'])
+    # print("R:", exp_dict[m][n_ep][db][rl][cw][lf]['R'])
 
 
 def delete(exp_dict, backup_dict):
@@ -203,7 +197,6 @@ def delete(exp_dict, backup_dict):
         if blah == True:
             break
 
-
         to_delete = exp_dict[inputs[0]][inputs[1]][inputs[2]][inputs[3]][inputs[4]].pop(inputs[5])
         backup_dict[inputs[0]][inputs[1]][inputs[2]][inputs[3]][inputs[4]][inputs[5]] = to_delete
         save(exp_dict, 'exp_dict.p')
@@ -211,9 +204,37 @@ def delete(exp_dict, backup_dict):
         done = input("Do you have another entry to delete? [y, n] ")
         done = True if done == 'y' else False
 
+def show_remaining_exps(exp_dict):
+    '''
+        # >>> from beautifultable import BeautifulTable
+        # >>> table = BeautifulTable()
+        # >>> table.rows.append(["Jacob", 1, "boy"])
+        # >>> table.rows.append(["Isabella", 1, "girl"])
+        # >>> table.rows.append(["Ethan", 2, "boy"])
+        # >>> table.rows.append(["Sophia", 2, "girl"])
+        # >>> table.rows.append(["Michael", 3, "boy"])
+        # >>> table.rows.header = ["S1", "S2", "S3", "S4", "S5"]
+        # >>> table.columns.header = ["name", "rank", "gender"]
+        # >>> print(table)
+        :param exp_dict:
+        :return:
+    '''
+    table = BeautifulTable(maxwidth=300)
+    table.columns.header = ["Model", "#Epochs", "Doc Batching", "Ranking Loss", "Class Weights", "Loss Funct"]
+    table.column_widths = [17, 9, 20, 23, 23, 15]
+    print("######################################## Experiments to Run ########################################")
+    for m in models:
+        for n_ep in num_epohcs:
+            for db in doc_batching:
+                for rl in ranking_loss:
+                    for cw in class_weights:
+                        for lf in loss_function:
+                            if not exp_dict[m][n_ep][db][rl][cw][lf]['MAP']:
+                                table.rows.append([m, n_ep, db, rl, cw, lf])
+    print(table)
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     try:
         exp_dict = load('exp_dict.p')
     except:
@@ -227,7 +248,13 @@ if __name__=='__main__':
 
     finished = False
     while not finished:
-        to_do = input("Enter new data, look up existing data, view all existing data, or delete an entry? [1, 2, 3, 4] ")
+        to_do = input(
+            "\t(1)\t Enter new data, "
+            "\n\t(2)\t look up existing data, "
+            "\n\t(3)\t view all existing data, "
+            "\n\t(4)\t delete an entry,"
+            "\n\t(5)\t or view possible experiments? "
+            "\n\t (hit any other key to quit)\n")
         if to_do == '1':
             fill_dict(exp_dict)
         elif to_do == '2':
@@ -245,17 +272,10 @@ if __name__=='__main__':
                     partial(defaultdict,
                     partial(defaultdict,
                     partial(defaultdict, float)))))))
-            print(backup_dict)
             delete(exp_dict, backup_dict)
-        elif to_do not in ['1', '2', '3']:
+        elif to_do == '5':
+            show_remaining_exps(exp_dict)
+        else:
             finished = True
 
-    # print("######################################## Experiments to Run ########################################")
-    # for m in models:
-    #     for n_ep in num_epohcs:
-    #         for db in doc_batching:
-    #             for rl in ranking_loss:
-    #                 for cw in class_weights:
-    #                     for lf in loss_function:
-    #                         if not exp_dict[m][n_ep][db][rl][cw][lf]['MAP']:
-    #                             print(m + '\t' + n_ep + '\t' + db + '\t' + rl + '\t' + cw + '\t' + lf)
+
