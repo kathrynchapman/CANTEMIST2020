23bf2e2df3a7ecc5e7d37433c66b6addb82958dd
Kathryn Chapman <kathrynchapman@Kathryns-Air.fritz.box>
2020-09-22 13:58:59 +0200

    bug fixes

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # CANTEMIST2020
-This is the (my) repo for the CANTEMIST 2020 Shared Task - coding
+This is the repo for the CANTEMIST 2020 Shared Task - coding track
 
 To run the classifiers, one must first clone the official metric repo: https://github.com/TeMU-BSC/cantemist-evaluation-library
 ```
@@ -20,3 +20,25 @@ python run_classifier.py
       --doc_batching
 
  ```
+In order to completely replicate results, a pre-trained XLM-RoBERTa-Base model will need
+to be further pretrained using the MLM objective on all train, dev, and test data.
+The command for this is:
+```
+python run_language_modeling.py 
+      --output_dir futher_pretrained_xlmr_base-0/ 
+      --model_name_or_path xlm-roberta-base 
+      --model_type xlmroberta 
+      --train_data_file processed_data/cantemist/LM_xlm-roberta-base_150_75.txt 
+      --line_by_line
+      --block_size 150 
+      --mlm 
+      --msl 150 
+      --num_train_epochs 3 
+      --do_train
+```
+
+Then, the ```run_classifier.py``` script can be ran, changing the following:
+```
+      --model_type bert 
+      --model_name_or_path bert-base-multilingual-cased 
+```
--- create_language_modeling_files.py
+++ create_language_modeling_files.py
@@ -12,7 +12,6 @@ if not os.path.exists(out_path + cantemist_path):
     os.mkdir(out_path + cantemist_path)
 
 
-
 def save(fname, data):
     with open(fname, "wb") as wf:
         pickle.dump(data, wf)
@@ -21,13 +20,14 @@ def save(fname, data):
 class LanguageModelingDataReader():
     def __init__(self, args=''):
         self.args = args
-        self.train_path = os.path.join(cantemist_path, "train-set-to-publish/")
-        self.test_path = os.path.join(cantemist_path, "test-background-set-to-publish/")
-        self.dev_path = os.path.join(cantemist_path, "dev-set1-to-publish/")
+        self.train_path = os.path.join(cantemist_path, "train-set/")
+        self.test_path = os.path.join(cantemist_path, "test-set/")
+        self.background_path = os.path.join(cantemist_path, "background-set/")
+        self.dev_path = os.path.join(cantemist_path, "dev-set1/")
         self.data_list = []
         self.tokenizer = XLMRobertaTokenizer.from_pretrained(self.args.model_name_or_path)
 
-    def construct_data_dict(self, train=False, test=False):
+    def construct_data_dict(self, train=False, test=False, background=False):
         """
         Constructs the dictionary containing all data
         self.data_dict['doc_id'] = 'text'
@@ -37,34 +37,50 @@ class LanguageModelingDataReader():
         if train:
             path = self.train_path + 'cantemist-coding/txt/'
         elif test:
-            path = self.test_path
+            path = self.test_path + 'cantemist-ner/'
+        elif background:
+            path = self.background_path
         else:
             path = self.dev_path + 'cantemist-coding/txt/'
 
         all_file_names = os.listdir(path)
         for f_name in all_file_names:
-            with open(path + f_name) as f:
-                text = f.read().replace('\n', ' ').replace('\t', ' ')
+            if f_name[-3:] == 'txt':
+                with open(path + f_name) as f:
+                    text = f.read().replace('\n', ' ').replace('\t', ' ')
 
-                tokens = self.tokenizer.tokenize(text)
-                while tokens:
-                    self.data_list.append(self.tokenizer.convert_tokens_to_string(tokens[:self.args.msl]))
-                    tokens = tokens[self.args.msl - self.args.stride:]
+                    tokens = self.tokenizer.tokenize(text)
+                    while tokens:
+                        self.data_list.append(self.tokenizer.convert_tokens_to_string(tokens[:self.args.msl]))
+                        tokens = tokens[self.args.msl - self.args.stride:]
         self.data_list = [self.tokenizer.convert_tokens_to_string(t) for t in self.data_list]
 
     def write_data(self):
         with open(
-            out_path + cantemist_path + 'LM_{}_{}_{}.txt'.format(self.args.model_name_or_path, str(self.args.msl), str(self.args.stride)),
-            'w') as f:
+                out_path + cantemist_path + 'LM_{}_{}_{}.txt'.format(self.args.model_name_or_path, str(self.args.msl),
+                                                                     str(self.args.stride)),
+                'w') as f:
             for line in self.data_list:
                 f.write(line + '\n')
 
 
 if __name__ == '__main__':
+    """
+    Namespace(adam_epsilon=1e-08, block_size=150, cache_dir=None, config_name=None, 
+    device=device(type='cuda'), do_eval=False, do_train=True, eval_all_checkpoints=False, 
+    eval_data_file=None, evaluate_during_training=False, fp16=False, fp16_opt_level='O1', 
+    gradient_accumulation_steps=1, learning_rate=5e-05, line_by_line=True, local_rank=-1, 
+    logging_steps=500, max_grad_norm=1.0, max_steps=-1, mlm=True, mlm_probability=0.15, 
+    model_name_or_path='xlm-roberta-base', model_type='xlmroberta', msl=150, n_gpu=4, 
+    no_cuda=False, num_train_epochs=3.0, output_dir='futher_pretrained_xlmr_base-0/', 
+    overwrite_cache=False, overwrite_output_dir=False, per_gpu_eval_batch_size=4, per_gpu_train_batch_size=4, save_steps=500, save_total_limit=None, seed=42, server_ip='', server_port='', should_continue=False, stride=75, tokenizer_name=None, train_batch_size=16, train_data_file='processed_data/cantemist/LM_xlm-roberta-base_150_75.txt', warmup_steps=0, weight_decay=0.0)
+    """
+
     parser = argparse.ArgumentParser()
     parser.add_argument('--preprocess', action='store_true', help="Whether to redo all of the pre-processing.")
-    parser.add_argument('--model_name_or_path', type=str, help="What type of BERT flavor we're working with")
-    parser.add_argument('--msl', type=int, default=512, help="Intended max seq len")
+    parser.add_argument('--model_name_or_path', default='xlm-roberta-base', type=str,
+                        help="What type of BERT flavor we're working with")
+    parser.add_argument('--msl', type=int, default=150, help="Intended max seq len")
     parser.add_argument('--stride', type=int, default=75, help="Overlap between documents")
 
     args = parser.parse_args()
@@ -73,4 +89,5 @@ if __name__ == '__main__':
     reader.construct_data_dict(train=True)
     reader.construct_data_dict()
     reader.construct_data_dict(test=True)
+    reader.construct_data_dict(background=True)
     reader.write_data()
